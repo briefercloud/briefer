3812d8950c16b0d41d8b1c9af56d44b78f54df39
Lucas Vieira <vieiralucas4@gmail.com>
2024-08-28 12:20:52 -0300

    ping and get schema of psql and redshift using jupyter

--- apps/api/src/datasources/psql.ts
+++ apps/api/src/datasources/psql.ts
@@ -1,200 +1,27 @@
 import config from '../config/index.js'
-import prisma, {
-  DataSource,
-  PostgreSQLDataSource,
-  getPSQLCert,
-  getPSQLPassword,
-} from '@briefer/database'
-import pg from 'pg'
-import { logger } from '../logger.js'
-import { DataSourceConnectionError, DataSourceStructure } from '@briefer/types'
+import prisma, { DataSource, PostgreSQLDataSource } from '@briefer/database'
+import { DataSourceStructure } from '@briefer/types'
 import { DataSourceStatus } from './index.js'
+import { getPSQLSchema, pingPSQL } from '../python/query/psql.js'
 
-async function getPGConfig(
-  datasource: PostgreSQLDataSource
-): Promise<pg.ClientConfig> {
-  let ds = {
-    username: datasource.username,
-    host: datasource.host,
-    port: datasource.port,
-    database: datasource.database,
-  }
-
-  if (datasource.isDemo) {
-    const db = await prisma().postgreSQLDataSource.findFirstOrThrow({
-      where: { id: datasource.id },
-    })
-    ds = {
-      username: db.username,
-      host: db.host,
-      port: db.port,
-      database: db.database,
-    }
-  }
-
-  const password = encodeURIComponent(
-    await getPSQLPassword(datasource, config().DATASOURCES_ENCRYPTION_KEY)
-  )
-  const cert = await getPSQLCert(
-    datasource,
-    config().DATASOURCES_ENCRYPTION_KEY
-  )
-
-  const connectionString = `postgresql://${ds.username}:${password}@${ds.host}:${ds.port}/${ds.database}`
-
-  return {
-    connectionString,
-    ssl: cert
-      ? { ca: cert }
-      : // allow self-signed certificates
-        { rejectUnauthorized: false },
-  }
-}
-
-export async function ping(
-  datasource: PostgreSQLDataSource
-): Promise<DataSource> {
+export async function ping(ds: PostgreSQLDataSource): Promise<DataSource> {
   const lastConnection = new Date()
-  const pgConfig = await getPGConfig(datasource)
-
-  const err = await pingPSQLFromConfig(
-    pgConfig,
-    datasource.workspaceId,
-    datasource.id
-  )
+  const err = await pingPSQL(ds, 'psql', config().DATASOURCES_ENCRYPTION_KEY)
 
   if (!err) {
-    return updateConnStatus(datasource, {
+    return updateConnStatus(ds, {
       connStatus: 'online',
       lastConnection,
     })
   }
 
-  return updateConnStatus(datasource, { connStatus: 'offline', connError: err })
-}
-
-export async function pingPSQLFromConfig(
-  config: pg.ClientConfig,
-  workspaceId: string,
-  dataSourceId: string
-): Promise<DataSourceConnectionError | null> {
-  try {
-    const client = new pg.Client(config)
-
-    return await Promise.race([
-      new Promise<DataSourceConnectionError>((resolve) =>
-        setTimeout(
-          () =>
-            resolve({
-              name: 'TimeoutError',
-              message: 'Did not receive a response from PostgreSQL within 10s',
-            }),
-          10000 // 10s timeout
-        )
-      ),
-      new Promise<DataSourceConnectionError | null>(async (resolve) => {
-        try {
-          await client.connect()
-          await client.query('SELECT 1')
-          resolve(null)
-        } catch (err) {
-          logger.info({ err }, 'Error pinging PostgreSQL')
-          const parsedErr = DataSourceConnectionError.safeParse(err)
-          if (!parsedErr.success) {
-            logger.error(
-              {
-                error: err,
-                workspaceId,
-                dataSourceId,
-              },
-              'Failed to parse error from PostgreSQL ping'
-            )
-
-            resolve({ name: 'UnknownError', message: 'Unknown error' })
-            return
-          }
-
-          resolve(parsedErr.data)
-        } finally {
-          client.end()
-        }
-      }),
-    ])
-  } catch (err) {
-    logger.info({ err }, 'Error pinging PostgreSQL')
-    const parsedErr = DataSourceConnectionError.safeParse(err)
-    if (!parsedErr.success) {
-      logger.error(
-        {
-          error: err,
-          workspaceId,
-          dataSourceId,
-        },
-        'Failed to parse error from PostgreSQL ping'
-      )
-
-      return { name: 'UnknownError', message: 'Unknown error' }
-    }
-
-    return parsedErr.data
-  }
-}
-
-export async function getPSQLSchemaFromConfig(
-  datasourceId: string,
-  pgConfig: pg.ClientConfig
-): Promise<DataSourceStructure> {
-  const client = new pg.Client(pgConfig)
-  await client.connect()
-
-  // select all tables with their column names and types from all schemas
-  const res = await client.query(`
-    SELECT
-      table_schema,
-      table_name,
-      column_name,
-      data_type
-    FROM information_schema.columns
-    WHERE table_schema NOT IN ('information_schema', 'pg_catalog')
-    ORDER BY table_schema, table_name, ordinal_position
-  `)
-
-  const info: DataSourceStructure = {
-    dataSourceId: datasourceId,
-    schemas: {},
-    defaultSchema: 'public',
-  }
-  for (const row of res.rows) {
-    const schemaName = row.table_schema
-    const tableName = row.table_name
-    const columnName = row.column_name
-    const dataType = row.data_type
-
-    const schema = info.schemas[schemaName] ?? {
-      tables: {},
-    }
-
-    const table = schema.tables[tableName] ?? {
-      columns: [],
-    }
-
-    table.columns.push({ name: columnName, type: dataType })
-
-    schema.tables[tableName] = table
-    info.schemas[schemaName] = schema
-  }
-
-  await client.end()
-
-  return info
+  return updateConnStatus(ds, { connStatus: 'offline', connError: err })
 }
 
 export async function getSchema(
-  datasource: PostgreSQLDataSource
+  ds: PostgreSQLDataSource
 ): Promise<DataSourceStructure> {
-  const pgConfig = await getPGConfig(datasource)
-
-  return getPSQLSchemaFromConfig(datasource.id, pgConfig)
+  return getPSQLSchema(ds, 'psql', config().DATASOURCES_ENCRYPTION_KEY)
 }
 
 export async function updateConnStatus(
--- apps/api/src/datasources/redshift.ts
+++ apps/api/src/datasources/redshift.ts
@@ -1,113 +1,31 @@
-import pg from 'pg'
 import config from '../config/index.js'
-import prisma, {
-  DataSource,
-  RedshiftDataSource,
-  getRedshiftPassword,
-} from '@briefer/database'
-import { getPSQLSchemaFromConfig, pingPSQLFromConfig } from './psql.js'
+import prisma, { DataSource, RedshiftDataSource } from '@briefer/database'
 import { DataSourceStructure } from '@briefer/types'
 import { DataSourceStatus } from './index.js'
+import { getPSQLSchema, pingPSQL } from '../python/query/psql.js'
 
-async function getPGConfig(
-  datasource: RedshiftDataSource
-): Promise<pg.ClientConfig> {
-  const password = await getRedshiftPassword(
-    datasource,
+export async function ping(ds: RedshiftDataSource): Promise<DataSource> {
+  const lastConnection = new Date()
+  const err = await pingPSQL(
+    ds,
+    'redshift',
     config().DATASOURCES_ENCRYPTION_KEY
   )
 
-  return {
-    host: datasource.host,
-    database: datasource.database,
-    user: datasource.username,
-    password,
-    port: parseInt(datasource.port),
-  }
-}
-
-export async function ping(
-  datasource: RedshiftDataSource
-): Promise<DataSource> {
-  const lastConnection = new Date()
-
-  const pgConfig = await getPGConfig(datasource)
-
-  const err = await pingPSQLFromConfig(
-    pgConfig,
-    datasource.workspaceId,
-    datasource.id
-  )
   if (!err) {
-    return updateConnStatus(datasource, {
+    return updateConnStatus(ds, {
       connStatus: 'online',
       lastConnection,
     })
   }
 
-  return updateConnStatus(datasource, { connStatus: 'offline', connError: err })
+  return updateConnStatus(ds, { connStatus: 'offline', connError: err })
 }
 
 export async function getSchema(
-  datasource: RedshiftDataSource
+  ds: RedshiftDataSource
 ): Promise<DataSourceStructure> {
-  const pgConfig = await getPGConfig(datasource)
-
-  try {
-    return await getPSQLSchemaFromConfig(datasource.id, pgConfig)
-  } catch {}
-
-  const client = new pg.Client(pgConfig)
-  await client.connect()
-
-  // select all tables with their column names and types from all schemas
-  const res = await client.query(`
-SELECT
-    t.schemaname AS table_schema,
-    t.tablename AS table_name,
-    c.column AS column_name,
-    c.type AS data_type
-FROM
-    pg_catalog.pg_table_def c
-JOIN
-    pg_catalog.pg_tables t
-    ON c.tablename = t.tablename
-    AND c.schemaname = t.schemaname
-WHERE
-    t.tableowner != 'rdsdb'
-ORDER BY
-    t.schemaname,
-    t.tablename,
-    c.column`)
-
-  const info: DataSourceStructure = {
-    dataSourceId: datasource.id,
-    schemas: {},
-    defaultSchema: 'public',
-  }
-  for (const row of res.rows) {
-    const schemaName = row.table_schema
-    const tableName = row.table_name
-    const columnName = row.column_name
-    const dataType = row.data_type
-
-    const schema = info.schemas[schemaName] ?? {
-      tables: {},
-    }
-
-    const table = schema.tables[tableName] ?? {
-      columns: [],
-    }
-
-    table.columns.push({ name: columnName, type: dataType })
-
-    schema.tables[tableName] = table
-    info.schemas[schemaName] = schema
-  }
-
-  await client.end()
-
-  return info
+  return getPSQLSchema(ds, 'redshift', config().DATASOURCES_ENCRYPTION_KEY)
 }
 
 export async function updateConnStatus(
