d81445e96baa5a371be4691ef1e5ac05cbfc2d3e
AlexandreSenpai <alexandrebsramos@hotmail.com>
2024-09-22 00:18:15 -0300

    feat: add sql server python query handler

---
+++ apps/api/src/python/query/sqlserver.ts
@@ -0,0 +1,35 @@
+import { v4 as uuidv4 } from 'uuid'
+import { MySQLDataSource, getDatabaseURL } from '@briefer/database'
+import { RunQueryResult, SuccessRunQueryResult } from '@briefer/types'
+import { makeSQLAlchemyQuery } from './sqlalchemy.js'
+
+export async function makeSQLServerQuery(
+  workspaceId: string,
+  sessionId: string,
+  queryId: string,
+  dataframeName: string,
+  datasource: MySQLDataSource,
+  encryptionKey: string,
+  sql: string,
+  onProgress: (result: SuccessRunQueryResult) => void
+): Promise<[Promise<RunQueryResult>, () => Promise<void>]> {
+  const databaseUrl = await getDatabaseURL(
+    { type: 'sqlserver', data: datasource },
+    encryptionKey
+  )
+
+  const jobId = uuidv4()
+  const query = `${sql} -- Briefer jobId: ${jobId}`
+
+  return makeSQLAlchemyQuery(
+    workspaceId,
+    sessionId,
+    dataframeName,
+    databaseUrl,
+    'mysql',
+    jobId,
+    query,
+    queryId,
+    onProgress
+  )
+}
