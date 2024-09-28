20cb190d6bcf4939ebab7547e098f21220d9d2e3
Joe Zein <joe@soallabs.com>
2024-09-28 19:45:27 +0300

    Added the prisma migration file for the snowflake connector

---
+++ packages/database/prisma/migrations/20240928163915_introduce_snowflake_datasource/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "SnowflakeDataSource" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL,
+    "account" TEXT NOT NULL,
+    "user" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "warehouse" TEXT NOT NULL,
+    "database" TEXT NOT NULL,
+    "region" TEXT DEFAULT 'us-west',
+    "host" TEXT,
+    "notes" TEXT NOT NULL,
+    "structure" TEXT,
+    "isDemo" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "connStatus" "ConnStatus" NOT NULL DEFAULT 'online',
+    "connError" TEXT,
+    "lastConnection" TIMESTAMP(3),
+    "workspaceId" UUID NOT NULL,
+
+    CONSTRAINT "SnowflakeDataSource_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "SnowflakeDataSource" ADD CONSTRAINT "SnowflakeDataSource_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
