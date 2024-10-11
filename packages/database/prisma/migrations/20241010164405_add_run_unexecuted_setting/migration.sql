4ad3df80248f55a43e140411594a58a4e75686c4
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2024-10-10 21:02:47 -0300

    fix migration for unexecuted blocks

--- packages/database/prisma/migrations/20241010164405_add_run_unexecuted_setting/migration.sql
+++ packages/database/prisma/migrations/20241010164405_add_run_unexecuted_setting/migration.sql
@@ -1,9 +1,2 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `runUnexecutedblocks` on the `Document` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
-ALTER TABLE "Document" DROP COLUMN "runUnexecutedblocks",
-ADD COLUMN     "runUnexecutedBlocks" BOOLEAN NOT NULL DEFAULT true;
+ALTER TABLE "Document" ADD COLUMN "runUnexecutedBlocks" BOOLEAN NOT NULL DEFAULT true;
