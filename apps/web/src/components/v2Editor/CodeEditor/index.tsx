19afd29ed827d9cef9cfd90f446be6b27800a19c
Lucas Vieira <vieiralucas4@gmail.com>
2024-10-15 17:54:47 -0300

    add syntax highlight to codemirror

--- apps/web/src/components/v2Editor/CodeEditor/index.tsx
+++ apps/web/src/components/v2Editor/CodeEditor/index.tsx
@@ -1,64 +1,15 @@
 import * as Y from 'yjs'
 import { useCallback, useEffect, useRef } from 'react'
-import {
-  Annotation,
-  ChangeSpec,
-  EditorState,
-  StateField,
-} from '@codemirror/state'
+import { Annotation, EditorState, StateField } from '@codemirror/state'
 import { MergeView } from '@codemirror/merge'
 import { vscodeKeymap } from '@replit/codemirror-vscode-keymap'
 import { python } from '@codemirror/lang-python'
 import { sql } from '@codemirror/lang-sql'
-import {
-  EditorView,
-  rectangularSelection,
-  lineNumbers,
-  highlightActiveLineGutter,
-  highlightSpecialChars,
-  drawSelection,
-  highlightActiveLine,
-  dropCursor,
-  keymap,
-} from '@codemirror/view'
-import { history } from '@codemirror/commands'
-import {
-  indentOnInput,
-  foldGutter,
-  bracketMatching,
-  defaultHighlightStyle,
-  syntaxHighlighting,
-} from '@codemirror/language'
-import { highlightSelectionMatches } from '@codemirror/search'
-import { closeBrackets, autocompletion } from '@codemirror/autocomplete'
+import { basicSetup } from 'codemirror'
+import { EditorView, keymap } from '@codemirror/view'
+import { materialLight } from './theme'
+
 import useEditorAwareness from '@/hooks/useEditorAwareness'
-
-export const basicSetup = [
-  lineNumbers(),
-  highlightActiveLineGutter(),
-  highlightSpecialChars(),
-  history(),
-  foldGutter(),
-  drawSelection(),
-  dropCursor(),
-  EditorState.allowMultipleSelections.of(true),
-  indentOnInput(),
-  syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
-  bracketMatching(),
-  closeBrackets(),
-  autocompletion(),
-  rectangularSelection(),
-  highlightActiveLine(),
-  highlightSelectionMatches(),
-]
-
-export function getExtensions(language: 'python' | 'sql') {
-  return [
-    basicSetup,
-    keymap.of(vscodeKeymap),
-    ...(language === 'python' ? [python()] : language === 'sql' ? [sql()] : []),
-  ]
-}
 
 // FIXME: This is buggy
 export function createTextSync(source: Y.Text) {
@@ -67,7 +18,7 @@ export function createTextSync(source: Y.Text) {
       return source
     },
     update: (value: Y.Text, tr) => {
-      if (tr.annotation(IsLocalAnnotation)) {
+      if (tr.changes.empty || tr.annotation(IsLocalAnnotation)) {
         return value
       }
 
@@ -128,7 +79,6 @@ function brieferKeyMaps(cbs: {
         // shift + enter
         key: 'Shift-Enter',
         run: () => {
-          console.log('shift enter')
           cbs.onRunSelectNext()
           return true
         },
@@ -161,6 +111,7 @@ interface Props {
 }
 export function CodeEditor(props: Props) {
   const [editorState, editorAPI] = useEditorAwareness()
+
   const onRunInsertBlock = useCallback(() => {
     props.onRun()
     props.onInsertBlock()
@@ -172,7 +123,7 @@ export function CodeEditor(props: Props) {
   }, [props.onRun, editorAPI.move])
 
   const editorRef = useRef<HTMLDivElement>(null)
-  const editorViewRef = useRef<EditorView | null>(null)
+  const viewRef = useRef<EditorView | null>(null)
   const mergeViewRef = useRef<MergeView | null>(null)
 
   useEffect(() => {
@@ -180,135 +131,123 @@ export function CodeEditor(props: Props) {
       return
     }
 
-    const yTextSync = createTextSync(props.source)
-
-    const state = EditorState.create({
-      extensions: [
-        brieferKeyMaps({
+    function getExtensions(source: Y.Text) {
+      return [
+        ...brieferKeyMaps({
           onBlur: editorAPI.blur,
           onEditWithAI: props.onEditWithAI,
           onRun: props.onRun,
           onRunSelectNext,
           onRunInsertBlock,
         }),
-        ...getExtensions(props.language),
+        basicSetup,
+        EditorView.lineWrapping,
+        ...(props.language === 'python'
+          ? [python()]
+          : props.language === 'sql'
+            ? [sql()]
+            : []),
+        keymap.of(vscodeKeymap),
         EditorState.readOnly.of(props.readOnly),
-        yTextSync,
-      ],
-      doc: props.source.toString(),
-    })
+        createTextSync(source),
+        materialLight,
+      ]
+    }
+
+    function getSelection() {
+      const selection =
+        viewRef.current?.state.selection ??
+        mergeViewRef.current?.a.state.selection
+
+      const isOutOfRange =
+        selection &&
+        selection.ranges.some(
+          (range) =>
+            range.from > props.source.length || range.to > props.source.length
+        )
+
+      if (isOutOfRange) {
+        return undefined
+      }
+
+      return selection
+    }
+
+    function destroyCurrent() {
+      viewRef.current?.destroy()
+      viewRef.current = null
+      mergeViewRef.current?.destroy()
+      mergeViewRef.current = null
+    }
+
+    function initializeEditorView(parent: Element) {
+      const selection = getSelection()
+      destroyCurrent()
+
+      const state = EditorState.create({
+        extensions: getExtensions(props.source),
+        doc: props.source.toString(),
+        selection,
+      })
+
+      return new EditorView({
+        state,
+        parent,
+      })
+    }
+
+    function initializeMergeView(diff: Y.Text, parent: Element) {
+      const selection = getSelection()
+      destroyCurrent()
+
+      const a = EditorState.create({
+        extensions: getExtensions(props.source),
+        doc: props.source.toString(),
+        selection,
+      })
+      const b = EditorState.create({
+        extensions: getExtensions(diff),
+        doc: diff.toString(),
+      })
+
+      return new MergeView({
+        a,
+        b,
+        parent,
+      })
+    }
 
     if (props.diff) {
-      const b = EditorState.create({
-        extensions: [
-          ...getExtensions(props.language),
-          EditorState.readOnly.of(props.readOnly),
-          createTextSync(props.diff),
-        ],
-        doc: props.diff.toString(),
-      })
-      const mergeView = new MergeView({
-        a: state,
-        b,
-        parent: editorRef.current,
-      })
-      mergeViewRef.current = mergeView
-      return () => {
-        mergeView.destroy()
-      }
+      mergeViewRef.current = initializeMergeView(props.diff, editorRef.current)
+    } else {
+      viewRef.current = initializeEditorView(editorRef.current)
     }
-
-    const editorView = new EditorView({
-      state,
-      parent: editorRef.current,
-    })
-    editorViewRef.current = editorView
-    return () => {
-      editorView.destroy()
-    }
-  }, [
-    editorRef,
-    props.source,
-    editorAPI.blur,
-    props.onEditWithAI,
-    props.onRun,
-    onRunSelectNext,
-    onRunInsertBlock,
-    props.language,
-    props.readOnly,
-    props.diff,
-  ])
-
-  useEffect(() => {
-    if (!editorViewRef.current) {
-      return
-    }
-
-    const editorView = editorViewRef.current
-
-    const observer = (e: Y.YTextEvent, tr: Y.Transaction) => {
-      editorView.state.selection
-      if (tr.local) {
-        return
-      }
-
-      const changeSpecs: ChangeSpec[] = []
-      let pos = 0
-      for (const change of e.delta) {
-        if (change.insert) {
-          const text =
-            typeof change.insert === 'string'
-              ? change.insert
-              : Array.isArray(change.insert)
-              ? change.insert.join('')
-              : ''
-          changeSpecs.push({
-            from: pos,
-            insert: text,
-          })
-          pos += text.length
-        } else if (change.delete) {
-          changeSpecs.push({
-            from: pos,
-            to: pos + change.delete,
-            insert: '',
-          })
-          pos += change.delete
-        } else if (change.retain) {
-          pos += change.retain
-        }
-      }
-
-      const transaction = editorView.state.update({
-        changes: changeSpecs,
-        annotations: [IsLocalAnnotation.of(true)],
-      })
-
-      editorView.dispatch(transaction)
-    }
-
-    props.source.observe(observer)
-
-    return () => {
-      props.source.unobserve(observer)
-    }
-  }, [editorViewRef, props.source])
+  }, [props.source, props.diff, props.language, props.readOnly, editorRef])
 
   useEffect(() => {
     if (
       editorState.cursorBlockId === props.blockId &&
-      editorState.mode === 'insert' &&
-      editorViewRef.current &&
-      !editorViewRef.current.hasFocus
+      editorState.mode === 'insert'
     ) {
-      editorViewRef.current.focus()
+      if (viewRef.current && !viewRef.current.hasFocus) {
+        viewRef.current.focus()
+      } else if (
+        mergeViewRef.current &&
+        !mergeViewRef.current.a.hasFocus &&
+        !mergeViewRef.current.b.hasFocus
+      ) {
+        mergeViewRef.current.a.focus()
+      }
     }
-  }, [editorState, props.blockId, editorViewRef])
+  }, [editorState.cursorBlockId, editorState.mode, props.blockId])
 
-  const preventDefault = useCallback((e: React.MouseEvent) => {
-    e.stopPropagation()
-    editorAPI.insert(props.blockId, { scrollIntoView: false })
-  }, [])
+  const onClick = useCallback(
+    (e: React.MouseEvent) => {
+      e.stopPropagation()
+      editorAPI.insert(props.blockId, { scrollIntoView: false })
+    },
+    [editorAPI.insert, props.blockId]
+  )
 
-  return <div onClick={preventDefault} ref={editorRef}></div>
+  return <div onClick={onClick} ref={editorRef}></div>
 }
