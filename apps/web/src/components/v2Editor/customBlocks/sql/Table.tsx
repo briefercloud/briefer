02b1eaf32cb4a9cf014add488bd966ce4bf1c411
Lucas Vieira <vieiralucas4@gmail.com>
2024-11-07 10:51:23 -0300

    add icons to sql table columns

--- apps/web/src/components/v2Editor/customBlocks/sql/Table.tsx
+++ apps/web/src/components/v2Editor/customBlocks/sql/Table.tsx
@@ -1,3 +1,9 @@
+import {
+  Bars3BottomLeftIcon,
+  CalendarIcon,
+  FlagIcon,
+  HashtagIcon,
+} from '@heroicons/react/24/outline'
 import { DataFrameColumn, Json } from '@briefer/types'
 import clsx from 'clsx'
 import ScrollBar from '@/components/ScrollBar'
@@ -21,18 +27,24 @@ function Table(props: Props) {
       >
         <thead className="bg-gray-50 sticky top-0">
           <tr className="divide-x">
-            {props.columns.map((column, index) => (
-              <th
-                key={index}
-                scope="col"
-                className={clsx(
-                  props.isDashboard ? 'border-b' : 'border-y',
-                  'px-2 py-1.5 text-gray-500 whitespace-nowrap font-normal'
-                )}
-              >
-                {column.name}
-              </th>
-            ))}
+            {props.columns.map((column, index) => {
+              const Icon = getColumnTypeIcon(column.type)
+              return (
+                <th
+                  key={index}
+                  scope="col"
+                  className={clsx(
+                    props.isDashboard ? 'border-b' : 'border-y',
+                    'px-2 py-1.5 text-gray-500 whitespace-nowrap font-normal'
+                  )}
+                >
+                  <div className="flex space-x-1 items-center">
+                    <Icon className="h-3 w-3 text-gray-400" />
+                    <span>{column.name}</span>
+                  </div>
+                </th>
+              )
+            })}
           </tr>
         </thead>
         <tbody className="bg-white overflow-y-scroll max-h-[500px]">
@@ -63,6 +75,122 @@ function Table(props: Props) {
       </table>
     </ScrollBar>
   )
+}
+
+export function getColumnTypeIcon(
+  type: DataFrameColumn['type']
+): typeof FlagIcon {
+  switch (type) {
+    case 'byte':
+    case 'ubyte':
+    case 'short':
+    case 'ushort':
+    case 'i1':
+    case 'i2':
+    case 'i4':
+    case 'i8':
+    case 'int0':
+    case 'int':
+    case 'Int':
+    case 'int8':
+    case 'Int8':
+    case 'int16':
+    case 'Int16':
+    case 'int32':
+    case 'Int32':
+    case 'int64':
+    case 'Int64':
+    case 'long':
+    case 'longlong':
+    case 'u1':
+    case 'u2':
+    case 'u4':
+    case 'u8':
+    case 'uint0':
+    case 'uint8':
+    case 'uint16':
+    case 'uint32':
+    case 'uint64':
+    case 'UInt0':
+    case 'UInt8':
+    case 'UInt16':
+    case 'UInt32':
+    case 'UInt64':
+    case 'uint':
+    case 'UInt':
+    case 'ulong':
+    case 'ULong':
+    case 'ulonglong':
+    case 'ULongLong':
+
+    case 'timedelta64':
+    case 'timedelta64[ns]':
+    case 'timedelta64[ns, UTC]':
+    case 'timedelta64[us]':
+    case 'timedelta64[us, UTC]':
+
+    case 'f2':
+    case 'f4':
+    case 'f8':
+    case 'f16':
+    case 'float16':
+    case 'float32':
+    case 'float64':
+    case 'float128':
+    case 'Float':
+    case 'Float16':
+    case 'Float32':
+    case 'Float64':
+    case 'float':
+    case 'longfloat':
+    case 'double':
+    case 'longdouble':
+      return HashtagIcon
+
+    case 'string':
+    case 'unicode':
+    case 'str':
+    case 'bytes':
+    case 'bytes0':
+    case 'str0':
+    case 'str':
+    case 'bytes':
+    case 'category':
+    case 'object':
+    case 'object0':
+      return Bars3BottomLeftIcon
+
+    case 'dbdate':
+    case 'dbtime':
+    case 'datetime64':
+    case 'datetime64[ns]':
+    case 'datetime64[ns, UTC]':
+    case 'datetime64[ns, Etc/UTC]':
+    case 'datetime64[us]':
+    case 'datetime64[us, UTC]':
+    case 'datetime64[us, Etc/UTC]':
+    case 'period':
+    case 'period[Y-DEC]':
+    case 'period[Q-DEC]':
+    case 'period[M]':
+    case 'period[Q]':
+    case 'period[W]':
+    case 'period[D]':
+    case 'period[h]':
+    case 'period[min]':
+    case 'period[m]':
+    case 'period[s]':
+    case 'period[ms]':
+    case 'period[us]':
+    case 'period[ns]':
+      return CalendarIcon
+
+    case 'bool':
+    case 'bool8':
+    case 'b1':
+    case 'boolean':
+      return FlagIcon
+  }
 }
 
 export default Table
