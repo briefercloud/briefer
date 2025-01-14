242e07a9aa7f4f51ff42f0a4db0e6a885d727c05
Lucas Vieira <vieiralucas4@gmail.com>
2025-01-14 12:36:20 -0300

    remove ydoc history when load starts to take too long

---
+++ packages/database/prisma/migrations/20250114143147_introduce_clock_updated_at/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "UserYjsAppDocument" ADD COLUMN     "clockUpdatedAt" TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "YjsAppDocument" ADD COLUMN     "clockUpdatedAt" TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "YjsDocument" ADD COLUMN     "clockUpdatedAt" TIMESTAMP(3);
