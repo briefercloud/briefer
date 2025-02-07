8afa793c9b252e8a8234ce76087db017d8c90cbb
Lucas Vieira <vieiralucas4@gmail.com>
2025-02-07 17:23:08 -0300

    fix is_categorical_dtype warning

--- apps/api/src/python/visualizations-v2.ts
+++ apps/api/src/python/visualizations-v2.ts
@@ -225,7 +225,7 @@ def _briefer_create_visualization(df, options):
                     df = df[df[column_name].isnull()]
                 elif operator == 'isNotNull':
                     df = df[df[column_name].notnull()]
-            elif pd.api.types.is_string_dtype(df[column_name]) or pd.api.types.is_categorical_dtype(df[column_name]) or pd.api.types.is_object_dtype(df[column_name]):
+            elif pd.api.types.is_string_dtype(df[column_name]) or isinstance(df[column_name].dtype, pd.CategoricalDtype) or pd.api.types.is_object_dtype(df[column_name]):
                 if operator == 'eq':
                     df = df[df[column_name] == value]
                 elif operator == 'ne':
