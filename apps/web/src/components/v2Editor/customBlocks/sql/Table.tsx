c092e368c788b8c80c650fc0fd00f6feb5fbe687
Lucas Vieira <vieiralucas4@gmail.com>
2025-02-07 11:05:13 -0300

    bg-gray-100 when hover table column head

--- apps/web/src/components/v2Editor/customBlocks/sql/Table.tsx
+++ apps/web/src/components/v2Editor/customBlocks/sql/Table.tsx
@@ -63,12 +63,10 @@ function Table(props: Props) {
                 <th
                   key={index}
                   scope="col"
-                  className="px-2 py-2 text-gray-500 whitespace-nowrap font-normal border-b"
+                  className="p-2 text-gray-500 whitespace-nowrap font-normal border-b hover:bg-gray-100 cursor-pointer"
+                  onClick={onChangeSort(column.name.toString())}
                 >
-                  <button
-                    className="flex space-x-2 items-center w-full justify-between"
-                    onClick={onChangeSort(column.name.toString())}
-                  >
+                  <div className="flex space-x-2 items-center w-full justify-between">
                     <div className="flex items-center space-x-1">
                       <Icon className="h-3 w-3 text-gray-400" />
                       <span>{column.name}</span>
@@ -84,7 +82,7 @@ function Table(props: Props) {
                     ) : (
                       <ArrowsUpDownIcon className="w-3 h-3 text-gray-300" />
                     )}
-                  </button>
+                  </div>
                 </th>
               )
             })}
